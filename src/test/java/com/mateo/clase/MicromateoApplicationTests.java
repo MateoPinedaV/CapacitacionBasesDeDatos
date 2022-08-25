b5c16ef3013b1998fe0afeb06e3ecaf49410067f
mapvv2003 <mapvv2003@gmail.com>
2022-08-25 17:52:53 -0500

    subir

---
+++ src/test/java/com/mateo/clase/MicromateoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateo.clase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicromateoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
